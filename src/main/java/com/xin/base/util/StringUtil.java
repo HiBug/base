071931b332bf34e53cff85fb28213c98a4213575
three <690403568@qq.com>
2019-01-15 14:21:22 +0800

    StringUtil

---
+++ src/main/java/com/xin/base/util/StringUtil.java
@@ -0,0 +1,13 @@
+package com.xin.base.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @author three
+ * @since 2019/1/15 14:20
+ * <p>
+ *
+ * </p>
+ */
+public class StringUtil extends StringUtils {
+}
